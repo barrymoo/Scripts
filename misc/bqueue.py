eb91e5d6c0019930f3218d000c090e6dd8e26202
Barry <moore0557@gmail.com>
2013-04-17 09:29:48 -0400

    update bqueue.py

--- misc/bqueue.py
+++ misc/bqueue.py
@@ -2,5 +2,5 @@
 import os
 
 userName = os.environ['USER']
-os.system('squeue -u ' + userName + ' -o "%6i %3P %35j %2t %.12M %.6D %.4C %.R"')
+os.system('squeue -u ' + userName + ' -o "%6i %3P %65j %2t %.12M %.6D %.4C %.R"')
 
