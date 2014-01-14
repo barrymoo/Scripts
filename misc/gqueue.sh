dea1943e07309cf1f730db7889d0baaeb2752b1a
Barry <moore0557@gmail.com>
2014-01-14 15:20:55 -0500

    Add watch option to bqueue and gqueue

--- misc/gqueue.sh
+++ misc/gqueue.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 GROUP=`groups | cut -d\  -f1`
-squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"
+if [$1 == "watch"]; then
+    watch squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R" --sort=S
+else
+    squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"
+fi
