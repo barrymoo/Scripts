3d9c01bf6b6518d167ed19f85b0e7f7c015954cb
Barry <moore0557@gmail.com>
2014-01-14 15:27:54 -0500

    Watch Option Mistake

--- misc/bqueue.sh
+++ misc/bqueue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ["$1" = "watch"]; then
+if [ "$1" = "watch" ]; then
     watch squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R" --sort=S
 else
     squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R"
--- misc/gqueue.sh
+++ misc/gqueue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GROUP=`groups | cut -d\  -f1`
-if ["$1" = "watch"]; then
+if [ "$1" = "watch" ]; then
     watch squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R" --sort=S
 else
     squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"
