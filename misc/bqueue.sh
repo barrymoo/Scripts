effda93b345a37b2ce936a47ab6b05b1691561f4
Barry <moore0557@gmail.com>
2014-01-14 15:23:10 -0500

    Watch Option Mistake

--- misc/bqueue.sh
+++ misc/bqueue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [$1 -eq "watch"]; then
-    watch squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R" --sort=S
+    /usr/bin/watch squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R" --sort=S
 else
     squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R"
 fi
