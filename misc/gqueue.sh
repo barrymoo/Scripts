b4b88579a4bed575594ae1b65f4c23054896594d
Barry <moore0557@gmail.com>
2014-01-14 15:33:17 -0500

    Watch Option Mistake

--- misc/gqueue.sh
+++ misc/gqueue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 GROUP=`groups | cut -d\  -f1`
-if [ "$1" = "watch" ]; then
-    watch squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R" --sort=S
+if [ "$1" = "w" ]; then
+    watch 'squeue -M chemistry -p $GROUP -S S -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"'
 else
     squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"
 fi
