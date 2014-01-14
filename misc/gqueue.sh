c9934f498f47c3773b2ad5bb10dd27086ebcbca8
Barry <moore0557@gmail.com>
2014-01-14 15:37:05 -0500

    Update gqueue.sh -> quote issue

--- misc/gqueue.sh
+++ misc/gqueue.sh
@@ -2,7 +2,7 @@
 
 GROUP=`groups | cut -d\  -f1`
 if [ "$1" = "w" ]; then
-    watch 'squeue -M chemistry -p $GROUP -S S -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"'
+    watch "squeue -M chemistry -p $GROUP -S S -o '%7i %30j %3u %2t %.12M %.6D %.4C %.R'"
 else
     squeue -M chemistry -p $GROUP -o "%7i %30j %3u %2t %.12M %.6D %.4C %.R"
 fi
