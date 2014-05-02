0a8b18d8e227a8a624522af880940234854d1c83
Barry <moore0557@gmail.com>
2014-05-02 17:12:48 -0400

    Fix sorting issue with cubefilelist.py

--- nwchem-tools/cube2t41/cubefilelist.py
+++ nwchem-tools/cube2t41/cubefilelist.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
-import glob
+from glob import glob
+from re import split
+from operator import itemgetter
 
-cubeFiles = glob.glob("*cube")
-cubeFiles.sort()
+cubeFiles = glob("*cube")
+for i in range(len(cubeFiles)):
+    sp = split('-|\.', cubeFiles[i])
+    cubeFiles[i] = [sp[0], int(sp[1]), sp[2]]
+
+cubeFiles = sorted(cubeFiles, key=itemgetter(1))
+
 f = open('cubefiles', 'w')
-f.write(str(len(cubeFiles)) + '\n')
+f.write("{0!s}\n".format(len(cubeFiles)))
 for i in cubeFiles:
-    f.write(i + '\n')
+    f.write("{0}-{1!s}.{2}\n".format(i[0], i[1], i[2]))
