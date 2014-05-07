2bddf4d33d6f1cb1dbba5651f274a731cbba75b2
Barry <moore0557@gmail.com>
2014-05-07 13:54:17 -0400

    fix cubefilelist.py for multiple dashes

--- nwchem-tools/cube2t41/cubefilelist.py
+++ nwchem-tools/cube2t41/cubefilelist.py
@@ -2,15 +2,18 @@
 from glob import glob
 from re import split
 from operator import itemgetter
+from sys import exit
 
 cubeFiles = glob("*cube")
 for i in range(len(cubeFiles)):
-    sp = split('-|\.', cubeFiles[i])
-    cubeFiles[i] = [sp[0], int(sp[1]), sp[2]]
+    sp = cubeFiles[i].split('-')
+    prefix = '-'.join(sp[0:-1])
+    num = sp[-1].replace('.cube','')
+    cubeFiles[i] = [prefix, int(num)]
 
 cubeFiles = sorted(cubeFiles, key=itemgetter(1))
 
 f = open('cubefiles', 'w')
 f.write("{0!s}\n".format(len(cubeFiles)))
 for i in cubeFiles:
-    f.write("{0}-{1!s}.{2}\n".format(i[0], i[1], i[2]))
+    f.write("{0}-{1!s}.cube\n".format(i[0], i[1]))
