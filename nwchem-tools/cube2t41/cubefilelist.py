21e65fb9f89ddeae1daacf30bf317ec372142f36
Barry <moore0557@gmail.com>
2014-07-16 13:08:28 -0400

    Fix natural sort algorithm implemented in cubefilelist.py

--- nwchem-tools/cube2t41/cubefilelist.py
+++ nwchem-tools/cube2t41/cubefilelist.py
@@ -5,8 +5,8 @@ from sys import exit
 
 # This function is natural sorting algorithm
 def natural_sort(fileList):
-    is_int = lambda text: int(text) if re.match('\d', text) else text.lower()
-    natural_key = lambda key: [is_int(x) for x in re.split('(\d+)', key)[-1]]
+    is_int = lambda text: int(text) if re.match('\d', text) else False
+    natural_key = lambda key: [is_int(x) for x in re.split('(\d+)', key)]
     return sorted(fileList, key = natural_key)
 
 # Attempt to generate a list of cubefiles
@@ -16,8 +16,12 @@ cubeFiles = glob("*.cube")
 if not len(cubeFiles) > 0:
     exit('Error: There are no .cube files in this directory?')
 
+print(cubeFiles)
+
 # Use our natural sort algorithm to sort the files
 cubeFiles = natural_sort(cubeFiles)
+
+print(cubeFiles)
 
 # Finally write the cubefiles file if the number of cubes
 #  is greater than 0
