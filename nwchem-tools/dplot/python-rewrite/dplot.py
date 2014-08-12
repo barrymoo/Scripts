cc964515e5a299332b27053d9f8ff6ae3b88c3e1
Barry <moore0557@gmail.com>
2014-08-12 13:23:47 -0400

    Add density option to dplot.py

--- nwchem-tools/dplot/python-rewrite/dplot.py
+++ nwchem-tools/dplot/python-rewrite/dplot.py
@@ -20,6 +20,8 @@ Options:
     -c, --civecs <nwchem.civecs>    Print Transition Densities (TD) instead of MOs
     -g, --grid <value>              Change default grid parameter [default=50]
     -e, --extent <value>            Change default extent of box size [default=2.0]
+    -d, --density <string>          Plot the density (ignores -l/--list)
+                                        valid strings include: total, alpha, beta, spin
 """
 
 
@@ -70,7 +72,7 @@ try:
     # Input Check 1: Can not use -b/--beta or -a/--alpha-beta with -c/--civecs
     #   No reason to move on if this is incorrect
     if arguments['--civecs'] and (arguments['--beta'] or arguments['--alpha-beta']):
-        exit('Input Error: -b/--beta and -a/--alpha-beta options cannot be used with -c/--civecs option')
+        raise Exception('Input Error: -b/--beta and -a/--alpha-beta options cannot be used with -c/--civecs option')
 
     # Input Check 2: does '--input' file exists, if not continue to propmt user until they enter a file that exists
     while True:
@@ -110,7 +112,11 @@ try:
         else:
             arguments['--extent'] = 2.0
     except ValueError:
-        exit("Error: The '-e/--extent' option accepts a float value only!") 
+        raise Exception("Extent Error: The '-e/--extent' option accepts a float value only!") 
+
+    # Input Check 7: Is density switch set, if yes is it a valid string?
+    if arguments['--density'] not in ['total', 'alpha', 'beta', 'spin']:
+        raise Exception('Density Error: {} is not a valid option, try -h/--help'.format(arguments['--density']))
     
     # Input file exists, let's read it
     with open(arguments['--input'], 'r') as f:
@@ -151,44 +157,55 @@ try:
                 f.write(i)
         
         # now we can add the dplot blocks!
-        #   Need some logic here for the '--alpha-beta' and '--beta' options
+        #   --> if '--density' is set, we will only process one block
+        #   --> need some logic here for the '--alpha-beta' and '--beta' options
         base = splitext(arguments['--input'])[0]
         mos = split_mo_list(arguments['--list'])
-        if arguments['--alpha-beta']:
-            for spin in ['alpha', 'beta']:
+        if arguments['--density']:
+            f.write('\n')
+            begin_dplot_block(f, arguments, x, y, z)
+            f.write('spin {}\n'.format(arguments['--density']))
+            f.write('output {}-{}-dens.cube\n'.format(base, arguments['--density']))
+            end_dplot_block(f)
+        else:
+            if arguments['--alpha-beta']:
+                for spin in ['alpha', 'beta']:
+                    for i in mos:
+                        f.write('\n')
+                        begin_dplot_block(f, arguments, x, y, z)
+                        f.write('spin {}\n'.format(spin))
+                        f.write('orbitals view;1;{}\n'.format(i))
+                        f.write('output {}-{}-{}.cube\n'.format(base, spin, i))
+                        end_dplot_block(f)
+            elif arguments['--beta']:
+                spin = 'beta'
                 for i in mos:
                     f.write('\n')
                     begin_dplot_block(f, arguments, x, y, z)
-                    f.write('spin {0}\n'.format(spin))
-                    f.write('orbitals view;1;{0}\n'.format(i))
-                    f.write('output {0}-{1}-{2}.cube\n'.format(base, spin, i))
+                    f.write('spin {}\n'.format(spin))
+                    f.write('orbitals view;1;{}\n'.format(i))
+                    f.write('output {}-{}.cube\n'.format(base, i))
                     end_dplot_block(f)
-        elif arguments['--beta']:
-            spin = 'beta'
-            for i in mos:
-                f.write('\n')
-                begin_dplot_block(f, arguments, x, y, z)
-                f.write('spin {0}\n'.format(spin))
-                f.write('orbitals view;1;{0}\n'.format(i))
-                f.write('output {0}-{1}.cube\n'.format(base, i))
-                end_dplot_block(f)
-        elif arguments['--civecs']:
-            for i in mos:
-                f.write('\n')
-                begin_dplot_block(f, arguments, x, y, z)
-                f.write('root {0}\n'.format(i))
-                f.write('output {0}-root-{1}.cube\n'.format(base, i))
-                f.write('civecs {0}\n'.format(arguments['--civecs']))
-                end_dplot_block(f)
-        else:
-            spin = 'alpha'
-            for i in mos:
-                f.write('\n')
-                begin_dplot_block(f, arguments, x, y, z)
-                f.write('spin {0}\n'.format(spin))
-                f.write('orbitals view;1;{0}\n'.format(i))
-                f.write('output {0}-{1}.cube\n'.format(base, i))
-                end_dplot_block(f)
+            elif arguments['--civecs']:
+                for i in mos:
+                    f.write('\n')
+                    begin_dplot_block(f, arguments, x, y, z)
+                    f.write('root {}\n'.format(i))
+                    f.write('output {}-root-{}.cube\n'.format(base, i))
+                    f.write('civecs {}\n'.format(arguments['--civecs']))
+                    end_dplot_block(f)
+            else:
+                spin = 'alpha'
+                for i in mos:
+                    f.write('\n')
+                    begin_dplot_block(f, arguments, x, y, z)
+                    f.write('spin {}\n'.format(spin))
+                    f.write('orbitals view;1;{}\n'.format(i))
+                    f.write('output {}-{}.cube\n'.format(base, i))
+                    end_dplot_block(f)
 
-except KeyboardInterrupt:
-    exit('Interrupt Detected! exiting...')
+except (KeyboardInterrupt, SystemExit):
+    raise Exception('Interrupt Detected! exiting...')
+
+except Exception as e:
+    print(e)
