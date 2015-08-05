7f072d0a9a76ed1cfb4fc051a1b5382c42516152
Barry Moore II <moore0557@gmail.com>
2015-08-05 09:51:29 -0400

    Fix bqueue.sh

--- misc/bqueue.sh
+++ misc/bqueue.sh
@@ -8,5 +8,5 @@ if [ "$1" = "-h" -o "$1" == "--help" ]; then
 elif [ "$1" = "-w" -o "$1" = "--watch" ]; then
      watch 'squeue -M chemistry,ub-hpc,industry -u $USER -S S -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R"'
 else
-    squeue -M chemistry,ub-hpc -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R"
+    squeue -M chemistry,ub-hpc,industry -u $USER -o "%7i %3P %35j %2t %.12M %.6D %.4C %.R"
 fi
